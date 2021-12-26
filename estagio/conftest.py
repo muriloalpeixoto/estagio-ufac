406c24c0dcf5055266506763f28924884c731289
unknown <Murilo Alfonso Peixoto>
2021-12-26 20:48:53 -0300

    initital commit

---
+++ estagio/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from estagio.users.models import User
+from estagio.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
